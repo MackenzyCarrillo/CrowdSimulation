3afbca1ea3f1bca722477a7d3cf80d7c7d4655cb
MackenzyCarrillo <709666@student.dusd.net>
2024-03-13 13:47:44 -0700

    finished fleeing. startinng flocking

--- CrowdSimulationCarrillo/Assets/CreatingCityCrowd/AIControl.cs
+++ CrowdSimulationCarrillo/Assets/CreatingCityCrowd/AIControl.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
@@ -8,28 +8,58 @@ public class AIControl : MonoBehaviour {
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20.0f;
+    float fleeRadius = 10.0f;
 
-
-    // Use this for initialization
     void Start() {
-        goalLocations = GameObject.FindGameObjectsWithTag("goal");
-        agent = this.GetComponent<NavMeshAgent>();
-        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
-        anim = this.GetComponent<Animator>();
-        anim.SetFloat("wOffset",Random.Range(0, 1));
-        anim.SetTrigger("isWalking");
-        float sm = Random.Range(0.5f, 1.5f);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
 
+        agent = GetComponent<NavMeshAgent>();
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        ResetAgent();
     }
 
-    // Update is called once per frame
+    void ResetAgent() {
+
+        speedMult = Random.Range(0.5f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120.0f;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position) {
+
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius) {
+
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newGoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid) {
+
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10.0f;
+                agent.angularSpeed = 500.0f;
+            }
+        }
+    }
+
     void Update() {
 
-        if (agent.remainingDistance < 1) {
+        if (agent.remainingDistance < 1.0f) {
 
-            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+            ResetAgent();
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
         }
     }
 }
