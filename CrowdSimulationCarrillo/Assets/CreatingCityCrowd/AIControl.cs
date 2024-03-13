82fa38d9758e9d067dc2945612ca22c8317db1d5
MackenzyCarrillo <709666@student.dusd.net>
2024-03-13 12:56:37 -0700

    4.13 left in  creating a city crowd

--- CrowdSimulationCarrillo/Assets/CreatingCityCrowd/AIControl.cs
+++ CrowdSimulationCarrillo/Assets/CreatingCityCrowd/AIControl.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
     GameObject[] goalLocations;
-    UnityEngine.AI.NavMeshAgent agent;
+    NavMeshAgent agent;
     Animator anim;
 
 
     // Use this for initialization
     void Start() {
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
-        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent = this.GetComponent<NavMeshAgent>();
         agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
-       anim.SetFloat("wOffset",Random.Range(0, 1));
+        anim.SetFloat("wOffset",Random.Range(0, 1));
         anim.SetTrigger("isWalking");
+        float sm = Random.Range(0.5f, 1.5f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
 
     }
 
